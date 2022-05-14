5e551306fcc0ed051e76cd11234f489ceb60d77e
DSingh <DSingh@INHYDLDSINGH2.logisticsdev.intra>
2022-05-14 12:48:33 +0530

    Upload Chenge name

--- src/main/java/seleniumSession/UploadFiles.java
+++ src/main/java/seleniumSession/UploadFiles.java
@@ -22,7 +22,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class UploadFiles_UsingRobotClass {
+public class UploadFiles {
 
 	public static void main(String[] args) throws InterruptedException {
 
